b4a63ccbd91c007655533fda2886ff2c1a87a77a
Gleb Kuzmin <glebushkauzmin2015@gmail.com>
2023-09-04 22:24:51 +0500

    ну что то сделал, процентов 5

--- LR0/LR0.cs
+++ LR0/LR0.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Globalization;
+﻿using System.Globalization;
 
 Console.WriteLine("Type something!");
 var a = Console.ReadLine();
---
+++ LR1/LR1.cs
@@ -0,0 +1,56 @@
+﻿using Sharprompt;
+using System.Numerics;
+
+namespace LR1
+{
+    struct date
+    {
+        public int year;
+        public int month;
+        public int day;
+    }
+    struct student
+    {
+        public int number;
+        public string name;
+        public string surname;
+        public date birthdate;
+    }
+
+
+
+    public class group
+    {
+        public void dosomething() { Console.WriteLine("jopa"); }
+
+        public void displayStudents()
+        {
+
+        }
+
+        public void addStudent()
+        {
+            Console.WriteLine("adding new student...");
+        }
+    }
+
+    class LR1
+    {
+       static void Main()
+        {
+            bool flag = true;
+            student[] students; 
+
+            Console.WriteLine("Hello! Welcome to the group manager!");
+
+            while (flag == true)
+            {
+                var city = Prompt.Select("Select", new[] { "dosomething", "add student", "exit" });
+                var group = new group();
+                if (city == "dosomething") { group.dosomething(); }
+                if (city == "add student") { group.addStudent(); }
+                if (city == "exit") { flag = false; }
+            }
+        }
+    }
+}
