ee9832452ddb39b476ea902c533834184c900a61
Gleb Kuzmin <glebushkauzmin2015@gmail.com>
2023-09-04 20:37:10 +0500

    LR0

---
+++ LR0/LR0.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+Console.WriteLine("Type something!");
+var a = Console.ReadLine();
+Console.WriteLine($"you tiped: {a}");
+
