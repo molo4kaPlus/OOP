13d93da0d5bb2e45d0303f51c8e4342a223bad99
Gleb Kuzmin <glebushkauzmin2015@gmail.com>
2023-09-05 12:16:24 +0500

    ну что то уже работает

--- LR1/LR1.cs
+++ LR1/LR1.cs
@@ -3,34 +3,51 @@ using System.Numerics;
 
 namespace LR1
 {
-    struct date
+    public class student
     {
-        public int year;
-        public int month;
-        public int day;
-    }
-    struct student
-    {
-        public int number;
-        public string name;
-        public string surname;
-        public date birthdate;
-    }
-
-
-
-    public class group
-    {
-        public void dosomething() { Console.WriteLine("jopa"); }
-
-        public void displayStudents()
+        public string phone { get; set; }
+        public string fullName { get; set; }
+        public DateOnly birthDate { get; set; }
+        public override string ToString()
         {
+            return $"({fullName}) " + $"{birthDate:d}, " + $"{phone ?? "null"}";
+        }
+    }
 
+    public class studentList
+    {
+        private readonly List<student> _students = new List<student>();
+        public void displayAllStudents()
+        {
+            for (int i = 0; i < _students.Count; i++)
+            {
+                Console.WriteLine(_students[i]);
+            }
         }
 
-        public void addStudent()
+        public void addStudentFromCode(student student)
         {
-            Console.WriteLine("adding new student...");
+            if (student == null) { throw new ArgumentNullException(nameof(student)); }
+            if (string.IsNullOrEmpty(student.fullName)) { throw new ArgumentNullException(nameof(student.fullName)); }
+            _students.Add(student);
+        }
+
+        public void addStudentFromConsole()
+        {
+            Console.WriteLine("Enter full name,birthdate(YMD), phone number...");
+            student temp = new student
+            {
+                fullName = Console.ReadLine(),
+                birthDate = new DateOnly(
+                    Convert.ToInt32(Console.ReadLine()),
+                    Convert.ToInt32(Console.ReadLine()),
+                    Convert.ToInt32(Console.ReadLine())),
+                phone = Console.ReadLine()
+            };
+
+            if (temp == null) { throw new ArgumentNullException(nameof(student)); }
+            if (string.IsNullOrEmpty(temp.fullName)) { throw new ArgumentNullException(nameof(student.fullName)); }
+            _students.Add(temp);
         }
     }
 
@@ -43,13 +60,27 @@ namespace LR1
 
             Console.WriteLine("Hello! Welcome to the group manager!");
 
+            var list = new studentList();
+
+            list.addStudentFromCode(new student
+            {
+                fullName = "Кузьмин Глеб Олегович",
+                birthDate = new DateOnly(2000, 12, 8),
+                phone = "795181815625"
+            });
+            list.addStudentFromCode(new student
+            {
+                fullName = "Жмышенко Альберт АХмедович",
+                birthDate = new DateOnly(2003, 5, 8),
+                phone = "88005556565"
+            });
+
             while (flag == true)
             {
-                var city = Prompt.Select("Select", new[] { "dosomething", "add student", "exit" });
-                var group = new group();
-                if (city == "dosomething") { group.dosomething(); }
-                if (city == "add student") { group.addStudent(); }
-                if (city == "exit") { flag = false; }
+                var menu = Prompt.Select("Select", new[] { "Add new student", "Display all students", "exit" });
+                if (menu == "Add new student") { list.addStudentFromConsole(); }
+                if (menu == "Display all students") { list.displayAllStudents(); }
+                if (menu == "exit") { flag = false; }
             }
         }
     }
