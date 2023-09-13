01e4b5170b1edf12fe05f1d5e2b0449e3f124e76
Gleb Kuzmin <glebushkauzmin2015@gmail.com>
2023-09-13 15:03:26 +0500

    структура сделана!

--- LR2/LR2.cs
+++ LR2/LR2.cs
@@ -6,9 +6,8 @@ namespace LR2
 {
     public class array
     {
-        private readonly int[] _array;
-        public array(params int[] array) { _array = array;  }
-        //public array(int[] array, int number) { array.Append(number);  _array = array; }
+        private readonly List<int> _array;
+        public array(List<int> array) { _array = array;  }
         public override string ToString()
         {
             string str = "";
@@ -20,15 +19,20 @@ namespace LR2
         }
         public array Add(int ToAdd)
         {
-            _array.Append(ToAdd);
-            Console.WriteLine(_array.ToString());
+            _array.Add(ToAdd);
             return new array(_array);
         }
 
-        public static array operator +(array arrayA, array arrayB)
+        public array Remove(int ToRemove) 
         {
-            return new array(0);
+
+            return new array(_array);
         }
+
+        //public static array operator +(array arrayA, array arrayB)
+        //{
+        //    return new array(0);
+        //}
     }
 
     class LR2
@@ -40,14 +44,13 @@ namespace LR2
             {
                 "Show array A",
                 "Show array B",
-                "Add a number",
+                "Add a number to array A",
+                "Add a number to array B",
                 "Exit"
             };
 
-            array arrayA = new array(1, 2, 3);
-            array arrayB = new array(2, 3, 4);
-            arrayA.Add(5);
-            arrayB.Add(6);
+            array arrayA = new array(new List<int> { 1, 2, 3 });
+            array arrayB = new array(new List<int> { 3, 2, 1 });
 
             Console.WriteLine("Welcome to the menu!");
             while (flag == true)
@@ -55,7 +58,9 @@ namespace LR2
                 var menu = Prompt.Select("Select", menuArguments);
                 if (menu == "Show array A") { Console.WriteLine(arrayA.ToString()); }
                 if (menu == "Show array B") { Console.WriteLine(arrayB.ToString()); }
-                if (menu == "Show array A and B") { Console.WriteLine(arrayA.ToString()); Console.WriteLine(arrayB.ToString); }
+                if (menu == "Add a number to array A") { arrayA.Add(Convert.ToInt32(Console.ReadLine())); }
+                if (menu == "Add a number to array B") { arrayB.Add(Convert.ToInt32(Console.ReadLine())); }
+                //if (menu == "Show array A and B") { Console.WriteLine(arrayA.ToString()); Console.WriteLine(arrayB.ToString); }
                 if (menu == "Exit") { flag = false; }
             }
         }
