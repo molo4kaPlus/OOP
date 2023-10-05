52e437225795f9f248095e583d8dfc57595ce68a
Gleb Kuzmin <glebushkauzmin2015@gmail.com>
2023-10-05 15:30:31 +0500

    LR3 start

---
+++ LR3/LR3.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+//стандартная библиотка .NET
+
+namespace LR3
+{
+    class LR3
+    {
+        public abstract class Figure
+        {
+            public double Thickness { get; }
+            public double Radius { get; }
+            public Tuple<double, double> Sides { get; }
+            public string FigureType { get; set; }
+
+            public Figure(double _radius, double _thickness) //Circle
+            {
+                Thickness = _thickness;
+                Radius = _radius;
+            }
+            public Figure(Tuple<double, double> _sides, double _thickness) //Square/rectangle/ellipse
+            {
+                Thickness = _thickness;
+                Sides = _sides;
+            }
+            public abstract double GetSpace();
+        }
+
+        class Square : Figure
+        {
+            public Square(double Thickness, Tuple<double, double> Sides) : base(Thickness, Sides) => FigureType = "Square";
+        }
+
+        static void Main()
+        {
+            
+        }
+    }
+}
