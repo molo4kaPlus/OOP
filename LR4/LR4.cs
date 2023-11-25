df26100a96d43847c3401b350822053d23505361
Gleb Kuzmin <glebushkauzmin2015@gmail.com>
2023-11-25 11:52:31 +0500

    jopa

--- LR4/LR4.cs
+++ LR4/LR4.cs
@@ -13,21 +13,23 @@ namespace LR4
         private readonly WebClient _webClient = new WebClient();
         private readonly HashSet<string> _ignoreFiles = new HashSet<string>() { ".ico", "xml"};
 
-        public event Action<Uri, List<string>> TargetFound;
+        public event Action<Uri, List<string>, int> TargetFound;
 
-        private void OnTargetFound(Uri page, List<string> links)
+        private void OnTargetFound(Uri page, List<string> links, int count)
         {
-            TargetFound?.Invoke(page, links);
+            TargetFound?.Invoke(page, links, count);
         }
         private void Process(string domain, Uri page, int count)
         {
             if (count <= 0) return;
             if (_procLinks.Contains(page)) return;
+            if (!isReachable(page)) { return; }
 
             _procLinks.Add(page);
+
             string html = _webClient.DownloadString(page);
 
-            var hrefs = (from href in Regex.Matches(html, @"src="".*?\.(jpg|jpeg|png)").Cast<Match>()
+            var hrefs = (from href in Regex.Matches(html, @"href=""[\/\w-\.:]+""").Cast<Match>()
                          let url = href.Value.Replace("href=", "").Trim('"')
                          let loc = url.StartsWith("/")
                          select new
@@ -37,10 +39,19 @@ namespace LR4
                          }
                          ).ToList();
 
+            var images = (from href in Regex.Matches(html, @"src="".*?\.(jpg|jpeg|png)""").Cast<Match>()
+                         let url = href.Value.Replace("href=", "").Trim('"')
+                         let loc = url.StartsWith("/")
+                         select new
+                         {
+                             Ref = loc ? $"https://mangalib.me{url}" : url,
+                             IsLocal = loc || url.StartsWith(domain)
+                         }
+                         ).ToList();
 
             List<string> externals = new List<string> { };
-            foreach (var href in hrefs) { if (!href.IsLocal) { externals.Add(href.Ref); } }
-            if (externals.Count > 0) OnTargetFound(page, externals);
+            foreach (var href in images) { if (!href.IsLocal) { externals.Add(href.Ref); } }
+            if (externals.Count > 0) OnTargetFound(page, externals, count);
             List<string> locals = new List<string> { };
             foreach (var href in hrefs) { if (href.IsLocal) { locals.Add(href.Ref); } }
 
@@ -50,6 +61,24 @@ namespace LR4
                 if (_ignoreFiles.Contains(fielEx)) continue;
 
                 Process(domain, new Uri(href), --count);
+            }
+        }
+
+        private bool isReachable(Uri url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Timeout = 15000;
+            request.Method = "HEAD";
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    return response.StatusCode == HttpStatusCode.OK;
+                }
+            }
+            catch (WebException)
+            {
+                return false;
             }
         }
         public void Scan(Uri startPage, int pageCount)
@@ -66,18 +95,25 @@ namespace LR4
     }
     class LR4
     {
+        public static void writeToCSV(List<List<string>> data, string path)
+        {
+
+        }
         static void Main()
         {
+            List<List<string>> jopa = new List<List<string>> { };
             Uri startPage = new Uri("https://mangalib.me/");
             using (WebScanner scanner = new WebScanner())
             {
-                scanner.TargetFound += (page, links) =>
+                scanner.TargetFound += (page, links, count) =>
                 {
-                    Console.WriteLine($"\nPage:\n\t{page}\nLinks:");
+                    jopa.Add(new List<string> { page.ToString(), count.ToString() });
+                    Console.WriteLine($"\nPage:\n\t{page}\nImages:");
                     foreach (var link in links)
                         Console.WriteLine($"\t{link}");
                 };
                 scanner.Scan(startPage, 30);
+                writeToCSV(jopa, "C:\\Users\\gleb\\Desktop");
                 Console.WriteLine("Done.");
             }
         }
